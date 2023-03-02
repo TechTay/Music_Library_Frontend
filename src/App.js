7597a24e610972b4dd432d62db80d5837b7d846e
DeVante Parker <Devante.parker15@gmail.com>
2023-03-02 15:59:54 -0600

    Initial Setup, Created axios Django REST API.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() =>{
+    getAllSongs();
+  }, []);
+
+  async function getAllSongs(){
+    const response = await axios.get('http://127.0.0.1:8000/api/songs/')
+    console.log(response.data)
+    setSongs(response.data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <button onClick={() => getAllSongs()}>Get All Songs</button>
     </div>
   );
 }
